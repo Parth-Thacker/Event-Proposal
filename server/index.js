f864c20e7baa8615ea1923f62e89464d4305ed35
Parth-Thacker <112555302+Parth-Thacker@users.noreply.github.com>
2023-03-01 15:00:29 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const Vendors = require("./model/vendor")
+const signUp = require("./route/signUp")
+const signIn = require("./route/signIn")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+
+var bodyParser = require('body-parser')
+const fileUpload = require("express-fileupload")
+app.use(fileUpload())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+const DB = 'mongodb+srv://parthThacker:parth1213@cluster0.twdoy22.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(DB).then(()=>{console.log("Connected");}).catch((err)=>{console.log(err.message);})
+//app.use(require("./route/Route"))
+app.use(express.json())
+app.use("/",signUp)
+// app.use("/",signIn)
+
+app.get("/",(req,res)=>{
+    res.send("ok")
+})
+
+app.listen(8080,()=>{console.log("Server is up")})
